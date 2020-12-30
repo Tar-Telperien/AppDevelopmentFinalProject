1e107bbe3530aaed6bb21333dc815e75399f7d48
Tar-Telperien <37634860+Tar-Telperien@users.noreply.github.com>
2020-12-30 11:23:35 -0600

    Import all app files

---
+++ TolkienCalendar/TolkienCalendar/DateViewController.swift
@@ -0,0 +1,51 @@
+//
+//  DateViewController.swift
+//  TolkienCalendar
+//
+//  Created by Amelia on 15/12/2019.
+//  Copyright © 2019 Amelia. All rights reserved.
+//
+
+import UIKit
+
+class DateViewController: UIViewController {
+    @IBOutlet weak var todayIsLabel: UILabel!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var languageSwitchButton: UIButton!
+    
+    var quenya: Bool = false
+    var date: String = "DATE"
+    let todayIs: String = "Today Is: "
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dateLabel.text = date
+        todayIsLabel.text = todayIs
+        todayIsLabel.font = UIFont(name: "Papyrus", size: 35)
+        dateLabel.font = UIFont(name: "Papyrus", size: 40)
+        languageSwitchButton.titleLabel?.text = "Tengwar"
+        languageSwitchButton.titleLabel?.font = UIFont(name: "TengwarEldamar", size: 35)
+        //languageSwitchButton.tintColor = .gray
+    }
+    
+    @IBAction func languageSwitchTapped(_ sender: UIButton) {
+        if (quenya == false) {
+            todayIsLabel.font = UIFont(name: "TengwarEldamar", size: 35)
+            dateLabel.font = UIFont(name: "TengwarEldamar", size: 40)
+            languageSwitchButton.titleLabel?.font = UIFont(name: "Papyrus", size: 35)
+            languageSwitchButton.titleLabel?.text = "English"
+            quenya = true
+        } else {
+            todayIsLabel.font = UIFont(name: "Papyrus", size: 35)
+            dateLabel.font = UIFont(name: "Papyrus", size: 40)
+            languageSwitchButton.titleLabel?.font = UIFont(name: "TengwarEldamar", size: 35)
+            languageSwitchButton.titleLabel?.text = "Tengwar"
+            quenya = false
+        }
+        self.view.reloadInputViews()
+    }
+}
---
+++ TolkienCalendar/TolkienCalendar/TolkienisedDate.swift
@@ -0,0 +1,78 @@
+//
+//  TolkienisedDate.swift
+//  TolkienCalendar
+//
+//  Created by Amelia on 10/12/2019.
+//  Copyright © 2019 Amelia. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class TolkienisedDate {
+    func dateTransform(date: Date) -> String {
+        var month: String
+        let formatter1 = DateFormatter()
+        formatter1.dateFormat = "MMMM"
+        month = formatter1.string(from: date)
+        
+        let formatter2 = DateFormatter()
+        formatter2.dateFormat = "d"
+        let day = formatter2.string(from: date)
+        
+        if (month == "January") {
+            if (day == "1"){
+                return "Yestarë"
+            }
+            else {
+                return "Narvinyë " + day
+            }
+        }
+        else if (month == "February") {
+            return "Nénimë " + day
+        }
+        else if (month == "March") {
+            return "Súlimë " + day
+        }
+        else if (month == "April") {
+            return "Víressë " + day
+        }
+        else if (month == "May") {
+            return "Lótessë " + day
+        }
+        else if (month == "June") {
+            if (day == "30"){
+                return "Loëndë"
+            }
+            else {
+                return "Nárië " + day
+            }
+        }
+        else if (month == "July") {
+            return "Cermië " + day
+        }
+        else if (month == "August") {
+            return "Urimë " + day
+        }
+        else if (month == "September") {
+            return "Yavannië " + day
+        }
+        else if (month == "October") {
+            return "Narquelië " + day
+        }
+        else if (month == "November") {
+            return "Hísimë " + day
+        }
+        else if (month == "December") {
+            if (day == "31"){
+                return "Mettarë"
+            }
+            else {
+                return "Ringarë " + day
+            }
+        }
+        else {
+            return "You have managed to discover a month not in the calendar. Congratulations on your ingenuity."
+        }
+    }
+}
